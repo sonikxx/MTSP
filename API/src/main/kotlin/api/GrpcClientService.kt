fb3e54753d3fa536c101ccb282b2031dfd0ebdab
sonikxx <shevlyakovasofia@icloud.com>
2025-03-29 14:35:10 +0300

    grpc streaming backup

--- API/src/main/kotlin/api/GrpcClientService.kt
+++ API/src/main/kotlin/api/GrpcClientService.kt
@@ -1,43 +1,101 @@
 package api
 
 import api.config.BackendConfig
-import api.dto.Point
-import api.dto.TspRequestDto
-import api.dto.TspResponseDto
+import api.dto.MtspRequestDto
 import com.example.grpc.City
-import io.grpc.ManagedChannelBuilder
+import com.example.grpc.MtspSolverRequest
 import org.springframework.stereotype.Service
-import com.example.grpc.TspSolverRequest
-import com.example.grpc.TspSolverGrpc
+import io.grpc.ManagedChannelBuilder
 import jakarta.annotation.PreDestroy
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
 import java.util.*
 
 
 @Service
 class GrpcClientService(
-    private val backendConfig: BackendConfig
+    backendConfig: BackendConfig,
 ) {
+
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
 
     private val grpcChannel = ManagedChannelBuilder.forAddress(backendConfig.mtsp.host, backendConfig.mtsp.port)
         .usePlaintext()  // Отключаем шифрование (для теста можно использовать)
         .build()
 
-    private val tspSolverStub = TspSolverGrpc.newBlockingStub(grpcChannel)
+    private val stub: MtspSolverGrpcKt.MtspSolverCoroutineStub =
+        MtspSolverGrpcKt.MtspSolverCoroutineStub(grpcChannel)
 
-    fun solveTsp(request: TspRequestDto): TspResponseDto {
-        val grpcRequest = TspSolverRequest.newBuilder()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    fun solveMtsp(httpRequest: MtspRequestDto) {
+        val rpcRequest = MtspSolverRequest.newBuilder()
             .setRequestId(UUID.randomUUID().toString())
-            .addAllCities(request.cities.map { City.newBuilder().setX(it.x).setY(it.y).build() })
+            .addAllCities(
+                httpRequest.cities.map { city ->
+                    City.newBuilder()
+                        .setX(city.x)
+                        .setY(city.y)
+                        .setName(city.name)
+                        .build()
+                }
+            )
+            .setNumSalesmen(httpRequest.salesmanNumber)
             .build()
 
-        val grpcResponse = tspSolverStub.solve(grpcRequest)
-
-        return TspResponseDto(grpcResponse.routeList.map { Point(it.x, it.y) })
+        coroutineScope.launch {
+            try {
+                stub.solve(rpcRequest).collect { response ->
+                    var logStr = ""
+                    for (route in response.routesList) {
+                        for (city in route.routeList) {
+                            logStr += city.name + " "
+                        }
+                        logStr += "| "
+                    }
+                    logger.info { "Received solution: $logStr" }
+                }
+            } catch (e: Exception) {
+                logger.error(e) { "Error calling gRPC server" }
+            }
+        }
     }
-
 
     @PreDestroy
     fun shutdown() {
         grpcChannel.shutdown()
     }
 }
+
+//@Service
+//class GrpcClientService(
+//    private val backendConfig: BackendConfig
+//) {
+//
+//    private val grpcChannel = ManagedChannelBuilder.forAddress(backendConfig.mtsp.host, backendConfig.mtsp.port)
+//        .usePlaintext()  // Отключаем шифрование (для теста можно использовать)
+//        .build()
+//
+//    private val tspSolverStub = TspSolverGrpc.newBlockingStub(grpcChannel)
+//
+//    fun solveTsp(request: TspRequestDto): TspResponseDto {
+//        val grpcRequest = TspSolverRequest.newBuilder()
+//            .setRequestId(UUID.randomUUID().toString())
+//            .addAllCities(request.cities.map { City.newBuilder().setX(it.x).setY(it.y).build() })
+//            .build()
+//
+//        val grpcResponse = tspSolverStub.solve(grpcRequest)
+//
+//        return TspResponseDto(grpcResponse.routeList.map { Point(it.x, it.y) })
+//    }
+//
+//
+//    @PreDestroy
+//    fun shutdown() {
+//        grpcChannel.shutdown()
+//    }
+//}
